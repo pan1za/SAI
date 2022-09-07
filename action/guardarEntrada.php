8c010d2b8505ac65060ec382eae434dfc0110857
Gustvo Paniza <panizagustavo@gmail.com>
2022-09-07 11:55:26 -0500

    agregar mensaje confirmacion ajax

--- action/guardarEntrada.php
+++ action/guardarEntrada.php
@@ -2,23 +2,46 @@
     session_start();
     include "../config/conexion.php";
 
-    if(isset($_POST["guardarEntrada"])){
-        $totalEntrada = $_POST["cantidad"];
-        $fechaEntrada = $_POST["fechaEntrada"];
-        $fechaVencimiento = $_POST["fechaVencimiento"];
-        $idProducto = $_POST["idProducto"];
-        $idUsuario = $_SESSION["user_id"];
+    $totalEntrada = $_POST["cantidad"];
+    $fechaEntrada = $_POST["fechaEntrada"];
+    $fechaVencimiento = $_POST["fechaVencimiento"];
+    $idProducto = $_POST["idProducto"];
+    $idUsuario = $_SESSION["user_id"];
 
-        $query = "INSERT INTO `entradas`(`totalEntrada`, `fechaEntrada`, `fechaVencimiento`, `idProducto`, `idUsuario`)
-        VALUES ('$totalEntrada', '$fechaEntrada','$fechaVencimiento', '$idProducto', $idUsuario)";
-        $result = mysqli_query($conn, $query);
+    $query = "INSERT INTO `entradas`(`totalEntrada`, `fechaEntrada`, `fechaVencimiento`, `idProducto`, `idUsuario`)
+            VALUES ('$totalEntrada', '$fechaEntrada','$fechaVencimiento', '$idProducto', $idUsuario)";
+    $result = mysqli_query($conn, $query);
 
-        if(!$result){
-            die("Hubo un error");
-        }
+    if ($result) {
+        $messages[] = "Producto registrado";
+    } else {
+        $errors[] = "Algo ha salido mal, intenta nuevamente." . mysqli_error($conn);
+    }
 
-        $_SESSION['message'] = 'Producto añadido correctamente';
-        $_SESSION['message_type'] = 'success';
-        header('Location: ../entrada.php');
+    if (isset($errors)){
+        ?>
+        <div class="alert alert-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <strong>Error!</strong>
+                <?php
+                    foreach ($errors as $error){
+                        echo $error;
+                    }
+                ?>
+        </div>
+        <?php
+    }
+    if (isset($messages)){
+        ?>
+        <div class="alert alert-success" role="alert">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <strong>¡Bien hecho!</strong>
+                <?php
+                    foreach ($messages as $message){
+                        echo $message;
+                    }
+                ?>
+        </div>
+    <?php
     }
 ?>
