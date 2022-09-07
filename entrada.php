50d6c34e1cf63d7f165eccb1cae85bf028923720
Gustvo Paniza <panizagustavo@gmail.com>
2022-09-07 11:53:04 -0500

    agregar mensaje confirmacion ajax

--- entrada.php
+++ entrada.php
@@ -8,12 +8,11 @@ include "config/conexion.php";
 </head>
 <?php include "include/navbar.php" ?>
 
-<!-- MESSAGE -->
-
 <main class="container p-4">
     <h3 class="offset-3 col-10">Registro de entradas</h3><br>
     <div class="card w-50 card-body offset-3 ">
-        <form action="action/guardarEntrada.php" method="POST">
+        <div id="result"></div>
+        <form id="agregarEntrada" name="agregarEntrada" method="POST">
             <div class="form-group mb-3">
                 <label class="form-label">Nombre del producto</label>
                 <select class="form-select" name="idProducto" required>
@@ -39,29 +38,29 @@ include "config/conexion.php";
                 <input type="date" name="fechaVencimiento" class="form-control" required>
             </div>
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button type="submit" class="btn btn-success bt-block" name="guardarEntrada">Guadar entrada</button>
+                <button type="submit" class="btn btn-success bt-block" id="guardarEntrada">Guadar entrada</button>
             </div>
         </form>
     </div>
 </main>
 
-<!-- <main class="container p-4">
-    <div class="card card-body offset-1 col-10">
-        <p>Registro de entradas</p>
-        <form action="/action/guardarEntrada.php" method="POST">
-            <div class="form-group mb-3">
-                <input type="text" name="nombreProducto" class="form-control" placeholder="Nombre del producto" autofocus>
-            </div>
-            <div class="form-group mb-3">
-                <input type="number" name="cantidad" class="form-control" placeholder="Cantidad">
-            </div>
-            <div class="form-group mb-3">
-                <input type="date" name="fechaEntrada" class="form-control">
-            </div>
-            <div class="form-group">
-                <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
-            </div>
-            <input type="submit" name="save_task" class="btn btn-success btn-block" value="Save Task">
-        </form>
-    </div>
-</main> -->
+<script>
+    $("#agregarEntrada").submit(function(event) {
+        $('#guardarEntrada').attr("disabled", true);
+
+        var parametros = $(this).serialize();
+        $.ajax({
+            type: "POST",
+            url: "action/guardarEntrada.php",
+            data: parametros,
+            beforeSend: function(objeto) {
+                $("#result").html("Mensaje: Cargando...");
+            },
+            success: function(datos) {
+                $("#result").html(datos);
+                $('#guardarEntrada').attr("disabled", false);
+            }
+        });
+        event.preventDefault();
+    })
+</script>
