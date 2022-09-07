e87c534a4d1fd0aa7525c54a347fee5f3428419b
Gustvo Paniza <panizagustavo@gmail.com>
2022-09-07 10:19:21 -0500

    agregar mensaje confirmacion ajax

--- action/guardarProducto.php
+++ action/guardarProducto.php
@@ -2,19 +2,45 @@
     session_start();
     include "../config/conexion.php";
 
-    if(isset($_POST["guardarProducto"])){
+ 
         $nombreProducto = $_POST["nombreProducto"];
         $unidadMedida = $_POST["unidadMedida"];
 
         $query = "INSERT INTO `productos`(`nombreProducto`, `unidadMedida`) VALUES ('$nombreProducto', '$unidadMedida')";
         $result = mysqli_query($conn, $query);
 
-        if(!$result){
-            die("Hubo un error");
-        }
+        if ($result){
+			$messages[] = "Producto registrado";
+		} else{
+			$errors []= "Algo ha salido mal, intenta nuevamente.".mysqli_error($conn);
+		}
 
-        $_SESSION['message'] = 'Producto añadido correctamente';
-        $_SESSION['message_type'] = 'success';
-        header('Location: ../producto.php');
-    }
+    
+
+    if (isset($errors)){
+        ?>
+        <div class="alert alert-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <strong>Error!</strong>
+                <?php
+                    foreach ($errors as $error){
+                        echo $error;
+                    }
+                    ?>
+        </div>
+        <?php
+        }
+        if (isset($messages)){
+            ?>
+            <div class="alert alert-success" role="alert">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>¡Bien hecho!</strong>
+                    <?php
+                        foreach ($messages as $message){
+                            echo $message;
+                        }
+                        ?>
+            </div>
+            <?php
+        }
 ?>
