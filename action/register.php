a0d7a4fd7cebae766bd050885e9e41e88ce2f71e
Gustvo Paniza <panizagustavo@gmail.com>
2022-09-07 10:19:51 -0500

    arreglo de cosas

--- action/register.php
+++ action/register.php
@@ -25,31 +25,34 @@
 			$errors []= "Algo ha salido mal, intenta nuevamente.".mysqli_error($conn);
 		}
 	}
-		if (isset($errors)){
-			?>
-			<div class="alert alert-danger" role="alert">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-					<strong>Error!</strong>
-					<?php
-						foreach ($errors as $error){
+	if (isset($errors)){
+			
+		?>
+		<div class="alert alert-danger" role="alert">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+				<strong>Error!</strong> 
+				<?php
+					foreach ($errors as $error) {
 							echo $error;
 						}
+					?>
+		</div>
+		<?php
+		}
+		if (isset($messages)){
+			
+			?>
+			<div class="alert alert-success" role="alert">
+					<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>¡Bien hecho!</strong>
+					<?php
+						foreach ($messages as $message) {
+								echo $message;
+							}
 						?>
 			</div>
 			<?php
-			}
-			if (isset($messages)){
-				?>
-				<div class="alert alert-success" role="alert">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<strong>¡Bien hecho!</strong>
-						<?php
-							foreach ($messages as $message){
-								echo $message;
-							}
-							?>
-				</div>
-				<?php
-			}
+		}
+
 
 ?>
