2b6acb560b22dfbac1a53bb23641bb263f7a6c59
Gustvo Paniza <panizagustavo@gmail.com>
2022-09-16 16:52:40 -0500

    add mostrar productos en stock

--- selectProductoSalida.php
+++ selectProductoSalida.php
@@ -4,18 +4,22 @@
     $idProducto = $_POST["idProducto"];
 
     if(isset($idProducto)){
-        //$query = "SELECT entradas.idEntrada FROM entradas WHERE entradas.idProducto = '$idProducto'";
-        $query = "SELECT * FROM entradas WHERE idProducto = '$idProducto'";
+        $query = "SELECT e.idEntrada, e.fechaEntrada, i.totalActual, p.unidadMedida
+        FROM entradas e
+        INNER JOIN productos p ON p.idProducto = e.idProducto
+        INNER JOIN inventario i ON i.idEntrada = e.idEntrada 
+        WHERE i.idProducto = '$idProducto'";
         $result = mysqli_query($conn,$query);
         $count = mysqli_num_rows($result);
 
         if(mysqli_num_rows($result)>0){
             echo '<option selected="selected" disabled value="">Seleccione una entrada</option>';
             while($row=mysqli_fetch_array($result)){
-                echo '<option value='.$row['idEntrada'].'>'.$row['fechaEntrada']. ' ('.$row['totalEntrada'].')' .'</option>';
+                echo '<option value='.$row['idEntrada'].'>'.'Entrada: '.$row['fechaEntrada']. ' - '.$row['totalActual'].' '.$row['unidadMedida'].'(s)'.' restantes'.'</option>';
             }
         }else{
             echo '<option selected disabled value="">No hay entradas de este producto</option>';
         }
     }
 ?>
+
