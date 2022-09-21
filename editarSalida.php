88e566accec93641fd3db17b7c5a81f4c78963c8
Gustvo Paniza <panizagustavo@gmail.com>
2022-09-21 16:53:08 -0500

    mostrar en input disabled el producto de la salida

--- editarSalida.php
+++ editarSalida.php
@@ -1,10 +1,7 @@
 <?php
 include "config/conexion.php";
-$totalEntrada = '';
-$fechaEntrada = '';
-$fechaVencimiento = '';
-
-//Terminar editar salida
+$totalSalida = '';
+$fechaSalida = '';
 
 if (isset($_GET['idSalida'])) {
     $idSalida = $_GET['idSalida'];
@@ -12,27 +9,25 @@ if (isset($_GET['idSalida'])) {
     $result = mysqli_query($conn, $query);
     if (mysqli_num_rows($result) == 1) {
         $row = mysqli_fetch_array($result);
-        $totalEntrada = $row['totalEntrada'];
-        $fechaEntrada = $row['fechaEntrada'];
-        $fechaVencimiento = $row['fechaVencimiento'];
+        $totalSalida = $row['totalSalida'];
+        $fechaSalida = $row['fechaSalida'];
     }
 }
 
 if (isset($_POST['update'])) {
     $idSalida = $_GET['idSalida'];
-    $totalEntrada = $_POST['totalEntrada'];
-    $fechaEntrada = $_POST['fechaEntrada'];
-    $fechaVencimiento = $_POST['fechaVencimiento'];
+    $totalSalida = $_POST['totalSalida'];
+    $fechaSalida = $_POST['fechaSalida'];
 
-    $query = "UPDATE entradas set totalEntrada = '$totalEntrada', fechaEntrada = '$fechaEntrada', fechaVencimiento = '$fechaVencimiento' WHERE idSalida=$idSalida";
+    $query = "UPDATE salidas set totalSalida = '$totalSalida', fechaSalida = '$fechaSalida' WHERE idSalida=$idSalida";
     mysqli_query($conn, $query);
-    $_SESSION['message'] = 'Producto editado correctamente';
+    $_SESSION['message'] = 'Salida editada correctamente';
     $_SESSION['message_type'] = 'warning';
-    header('Location: listaEntradas.php');
+    header('Location: listaSalidas.php');
 }
 
 if (isset($_POST['cancell'])) {
-    header('Location: listaEntradas.php');
+    header('Location: listaSalidas.php');
 }
 
 ?>
@@ -48,30 +43,58 @@ if (isset($_POST['cancell'])) {
     <h3 class="offset-3 col-10">Editar salida</h3><br>
     <div class="card w-50 card-body offset-3 ">
         <!-- <div id="result"></div> -->
-        <form action="editarEntrada.php?idSalida=<?php echo $_GET['idSalida']; ?>" method="POST">
+        <form action="editarSalida.php?idSalida=<?php echo $_GET['idSalida']; ?>" method="POST">
             <div class="form-group mb-3">
                 <label class="form-label">Nombre del producto</label>
                 <?php foreach ($conn->query("SELECT * from productos p 
-                                            INNER JOIN entradas e ON e.idProducto = p.idProducto WHERE e.idSalida = $idSalida") as $row) {
+                                            INNER JOIN salidas e ON e.idProducto = p.idProducto 
+                                            WHERE e.idSalida = $idSalida") as $row) {
                 ?>
                     <input type="text" class="form-control" disabled value="<?php echo $row['nombreProducto'], ' (' . $row['unidadMedida'] . ')' ?>">
                 <?php
                 }
                 ?>
+            </div>
+            
+            <div class="form-group mb-3">
+                <label class="form-label">Cantidad</label>
+                <input type="number" name="totalSalida" class="form-control" value="<?php echo $row['totalSalida'] ?>">
+            </div>
+            <div class="form-group mb-3">
+                <label for="fechaSalida" class="form-label">Fecha de salida</label>
+                <input type="date" name="fechaSalida" class="form-control" value="<?php echo $row['fechaSalida'] ?>">
+            </div>
 
-            </div>
+            <!-- si quito esto deja de funcionar el input disabled de abajo -->
+            <!-- <div class="form-group mb-3">
+                <label class="form-label">Entrada</label>
+                <?php foreach ($conn->query("SELECT * FROM entradas e
+                                            INNER JOIN productos p ON p.idProducto = e.idProducto
+                                            INNER JOIN salidas s ON s.idEntrada = s.idEntrada 
+                                            WHERE s.idSalida = $idSalida") as $row) {
+                ?>
+                    <input type="text" class="form-control" disabled value="<?php echo 'Entrada: '.$row['fechaEntrada'] . 'ID: '.$row['idEntrada']?>">
+                <?php
+                }
+                ?>
+            </div> -->
             <div class="form-group mb-3">
-                <label for="cantidad" class="form-label">Cantidad</label>
-                <input type="number" name="totalEntrada" class="form-control" value="<?php echo $row['totalEntrada'] ?>">
+                <?php
+                $query2 = "SELECT * FROM entradas e
+                INNER JOIN productos p ON p.idProducto = e.idProducto
+                INNER JOIN salidas s ON s.idEntrada = s.idEntrada 
+                WHERE s.idSalida = $idSalida";
+                $result2 = mysqli_query($conn,$query2);
+                $count2 = mysqli_num_rows($result2);
+
+                if(mysqli_num_rows($result2)>0){
+                    ?>
+                    <input type="text" class="form-control" disabled value="<?php echo  'Entrada: '.$row['fechaEntrada'] . ' - ID entrada: '.$row['idEntrada']?>">
+                <?php
+                }
+                ?>
             </div>
-            <div class="form-group mb-3">
-                <label for="fechaEntrada" class="form-label">Fecha de entrada</label>
-                <input type="date" name="fechaEntrada" class="form-control" value="<?php echo $row['fechaEntrada'] ?>">
-            </div>
-            <div class="form-group mb-3">
-                <label for="fechaVencimiento" class="form-label">Fecha de vencimiento</label>
-                <input type="date" name="fechaVencimiento" class="form-control" value="<?php echo $row['fechaVencimiento'] ?>">
-            </div>
+
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                 <button type="submit" class="btn btn-success bt-block" name="update">Update</button>
                 <button type="submit" class="btn btn-danger bt-block" name="cancell">Cancelar</button>
